22ad4f370fac158a987306a71e52be6a86c89722
luiz-ortega <luizngust@hotmail.com>
2020-10-11 18:20:42 -0300

    feat: move helicopter with keyboard.

--- 01-First_Helicopter_Game/js/js.js
+++ 01-First_Helicopter_Game/js/js.js
@@ -11,15 +11,55 @@ function start() {
 
     jogo.timer = setInterval(loop, 30);
 
+    var TECLA = {
+        W: 87,
+        S: 83,
+        D: 68
+    }
+
+    jogo.pressionou = [];
+
+    $(document).keydown(function (e) {
+        jogo.pressionou[e.which] = true;
+    });
+
+
+    $(document).keyup(function (e) {
+        jogo.pressionou[e.which] = false;
+    });
+
     function loop() {
 
         movefundo();
+        movejogador();
     }
 
     function movefundo() {
 
         esquerda = parseInt($("#fundoGame").css("background-position"));
         $("#fundoGame").css("background-position", esquerda - 1);
+    }
+
+    function movejogador() {
+
+        if (jogo.pressionou[TECLA.W]) {
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top", topo - 10);
+
+        }
+
+        if (jogo.pressionou[TECLA.S]) {
+
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top", topo + 10);
+        }
+
+        if (jogo.pressionou[TECLA.D]) {
+
+            //Chama funÁ„o Disparo	
+        }
 
     }
+
+
 }
