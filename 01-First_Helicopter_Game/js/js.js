5df5e2726edd5304515aa33f29bdb8840fd65147
luiz-ortega <luizngust@hotmail.com>
2020-10-11 18:22:49 -0300

    feat: limiting game frame boundaries.

--- 01-First_Helicopter_Game/js/js.js
+++ 01-First_Helicopter_Game/js/js.js
@@ -46,12 +46,22 @@ function start() {
             var topo = parseInt($("#jogador").css("top"));
             $("#jogador").css("top", topo - 10);
 
+            if (topo <= 0) {
+
+                $("#jogador").css("top", topo + 10);
+            }
+
         }
 
         if (jogo.pressionou[TECLA.S]) {
 
             var topo = parseInt($("#jogador").css("top"));
             $("#jogador").css("top", topo + 10);
+
+            if (topo >= 434) {
+                $("#jogador").css("top", topo - 10);
+
+            }
         }
 
         if (jogo.pressionou[TECLA.D]) {
