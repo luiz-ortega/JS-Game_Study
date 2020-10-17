b2c51a96274ba26d86cdf9f14a9a61070b17d5f3
luiz-ortega <luizngust@hotmail.com>
2020-10-17 15:48:35 -0300

    feat: cactus appearing randonly

--- 03-Dinossaur_Offline/script.js
+++ 03-Dinossaur_Offline/script.js
@@ -43,10 +43,23 @@ function jump() {
 function createCactus() {
   const cactus = document.createElement("div");
   let cactusPosition = 1000;
+  let randomTime = Math.random() * 3000;
 
   cactus.classList.add("cactus");
   cactus.style.left = 1000 + "px";
   background.appendChild(cactus);
+
+  let leftInterval = setInterval(() => {
+    if (cactusPosition < -60) {
+      clearInterval(leftInterval);
+      background.removeChild(cactus);
+    } else {
+      cactusPosition -= 10;
+      cactus.style.left = cactusPosition + "px";
+    }
+  }, 20);
+
+  setTimeout(createCactus, randomTime);
 }
 
 createCactus();
